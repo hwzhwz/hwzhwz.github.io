0f4954eb61314334a4cf45997f9fc0e9df48b8db
ssrv2ray <hovans>
2021-05-26 20:40:52 +0800

    Site updated: 2021-05-26 20:40:43

--- js/img-lazyload.js
+++ js/img-lazyload.js
@@ -1,10 +1,22 @@
-/* global Fluid, CONFIG */
-
-(function(window, document) {
-  for (const each of document.querySelectorAll('img[lazyload]')) {
-    Fluid.utils.waitElementVisible(each, function() {
-      each.removeAttribute('srcset');
-      each.removeAttribute('lazyload');
-    }, CONFIG.lazyload.offset_factor);
-  }
-})(window, document);
+'use strict';
+!function (t, e) {
+    var r = !0, l = !1, o = void 0;
+    try {
+        for (var i, a = function () {
+                    var t = i.value;
+                    Fluid.utils.waitElementVisible(t, function () {
+                        t.removeAttribute('srcset'), t.removeAttribute('lazyload');
+                    }, CONFIG.lazyload.offset_factor);
+                }, n = e.querySelectorAll('img[lazyload]')[Symbol.iterator](); !(r = (i = n.next()).done); r = !0)
+            a();
+    } catch (t) {
+        l = !0, o = t;
+    } finally {
+        try {
+            !r && n.return && n.return();
+        } finally {
+            if (l)
+                throw o;
+        }
+    }
+}(window, document);
--- js/utils.js
+++ js/utils.js
@@ -1,198 +1,93 @@
-/* global Fluid, CONFIG */
-
-window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
-
-Fluid.utils = {
-
-  listenScroll: function(callback) {
-    var dbc = new Debouncer(callback);
-    window.addEventListener('scroll', dbc, false);
-    dbc.handleEvent();
-    return dbc;
-  },
-
-  unlistenScroll: function(callback) {
-    window.removeEventListener('scroll', callback);
-  },
-
-  scrollToElement: function(target, offset) {
-    var of = jQuery(target).offset();
-    if (of) {
-      jQuery('html,body').animate({
-        scrollTop: of.top + (offset || 0),
-        easing   : 'swing'
-      });
-    }
-  },
-
-  elementInViewport: function(element, heightFactor) {
-    heightFactor = heightFactor || 1;
-    var rect = element.getBoundingClientRect();
-    var height = window.innerHeight || document.documentElement.clientHeight;
-    var top = rect.top;
-    return (top >= 0 && top <= height * (heightFactor + 1))
-      || (top <= 0 && top >= -(height * heightFactor) - rect.height);
-  },
-
-  waitElementVisible: function(selectorsOrElement, callback, heightFactor) {
-    var runningOnBrowser = typeof window !== 'undefined';
-    var isBot = (runningOnBrowser && !('onscroll' in window)) || (typeof navigator !== 'undefined'
-        && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent));
-    var supportsIntersectionObserver = 'IntersectionObserver' in window;
-
-    if (!runningOnBrowser || isBot) {
-      callback();
-      return;
-    }
-
-    var target;
-    if (typeof selectorsOrElement === 'string') {
-      target = document.querySelector(selectorsOrElement);
-    } else {
-      target = selectorsOrElement;
-    }
-
-    var _heightFactor = heightFactor || 2;
-
-    if (Fluid.utils.elementInViewport(target, _heightFactor)) {
-      callback();
-      return;
-    }
-
-    if (supportsIntersectionObserver) {
-      var io = new IntersectionObserver(function(entries, ob) {
-        if (entries[0].isIntersecting) {
-          callback();
-          ob.disconnect();
-        }
-      }, {
-        threshold : [0],
-        rootMargin: (window.innerHeight || document.documentElement.clientHeight) + 'px'
-      });
-      io.observe(target);
-    } else {
-      var warpCallback = Fluid.utils.listenScroll(function() {
-        if (Fluid.utils.elementInViewport(target, _heightFactor)) {
-          Fluid.utils.unlistenScroll(warpCallback);
-          callback();
-        }
-      });
-    }
-  },
-
-  waitElementLoaded: function(targetId, callback) {
-    var runningOnBrowser = typeof window !== 'undefined';
-    var isBot = (runningOnBrowser && !('onscroll' in window)) || (typeof navigator !== 'undefined'
-    && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent));
-
-    if (!runningOnBrowser || isBot) {
-      callback();
-      return;
-    }
-
-    if ('MutationObserver' in window) {
-      var mo = new MutationObserver(function(records, ob) {
-        var ele = document.getElementById(targetId);
-        if (ele) {
-          callback();
-          ob.disconnect();
-        }
-      });
-      mo.observe(document, { childList: true, subtree: true });
-    } else {
-      document.addEventListener('DOMContentLoaded', function() {
-        callback();
-      });
-    }
-  },
-
-  createScript: function(url, onload) {
-    var s = document.createElement('script');
-    s.setAttribute('src', url);
-    s.setAttribute('type', 'text/javascript');
-    s.setAttribute('charset', 'UTF-8');
-    s.async = false;
-    if (typeof onload === 'function') {
-      if (window.attachEvent) {
-        s.onreadystatechange = function() {
-          var e = s.readyState;
-          if (e === 'loaded' || e === 'complete') {
-            s.onreadystatechange = null;
-            onload();
-          }
-        };
-      } else {
-        s.onload = onload;
-      }
-    }
-    var e = document.getElementsByTagName('script')[0]
-    || document.getElementsByTagName('head')[0]
-    || document.head || document.documentElement;
-    e.parentNode.insertBefore(s, e);
-  },
-
-  createCssLink: function(url) {
-    var l = document.createElement('link');
-    l.setAttribute('rel', 'stylesheet');
-    l.setAttribute('type', 'text/css');
-    l.setAttribute('href', url);
-    var e = document.getElementsByTagName('link')[0]
-    || document.getElementsByTagName('head')[0]
-    || document.head || document.documentElement;
-    e.parentNode.insertBefore(l, e);
-  },
-
-  loadComments: function(selectors, loadFunc) {
-    var ele = document.querySelector('#comments[lazyload]');
-    if (ele) {
-      var callback = function() {
-        loadFunc();
-        ele.removeAttribute('lazyload');
-      };
-      Fluid.utils.waitElementVisible(selectors, callback, CONFIG.lazyload.offset_factor);
-    } else {
-      loadFunc();
-    }
-  }
-
-};
-
-/**
- * Handles debouncing of events via requestAnimationFrame
- * @see http://www.html5rocks.com/en/tutorials/speed/animations/
- * @param {Function} callback The callback to handle whichever event
- */
-function Debouncer(callback) {
-  this.callback = callback;
-  this.ticking = false;
+'use strict';
+function Debouncer(e) {
+    this.callback = e, this.ticking = !1;
 }
-Debouncer.prototype = {
-  constructor: Debouncer,
-
-  /**
-   * dispatches the event to the supplied callback
-   * @private
-   */
-  update: function() {
-    this.callback && this.callback();
-    this.ticking = false;
-  },
-
-  /**
-   * ensures events don't get stacked
-   * @private
-   */
-  requestTick: function() {
-    if (!this.ticking) {
-      requestAnimationFrame(this.rafCallback || (this.rafCallback = this.update.bind(this)));
-      this.ticking = true;
+window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame, Fluid.utils = {
+    listenScroll: function (e) {
+        var t = new Debouncer(e);
+        return window.addEventListener('scroll', t, !1), t.handleEvent(), t;
+    },
+    unlistenScroll: function (e) {
+        window.removeEventListener('scroll', e);
+    },
+    scrollToElement: function (e, t) {
+        var n = jQuery(e).offset();
+        n && jQuery('html,body').animate({
+            scrollTop: n.top + (t || 0),
+            easing: 'swing'
+        });
+    },
+    elementInViewport: function (e, t) {
+        t = t || 1;
+        var n = e.getBoundingClientRect(), i = window.innerHeight || document.documentElement.clientHeight, o = n.top;
+        return 0 <= o && o <= i * (t + 1) || o <= 0 && o >= -i * t - n.height;
+    },
+    waitElementVisible: function (e, n, t) {
+        var i = 'undefined' != typeof window, o = i && !('onscroll' in window) || 'undefined' != typeof navigator && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent), r = 'IntersectionObserver' in window;
+        if (i && !o) {
+            var a;
+            a = 'string' == typeof e ? document.querySelector(e) : e;
+            var c = t || 2;
+            if (Fluid.utils.elementInViewport(a, c))
+                n();
+            else if (r)
+                new IntersectionObserver(function (e, t) {
+                    e[0].isIntersecting && (n(), t.disconnect());
+                }, {
+                    threshold: [0],
+                    rootMargin: (window.innerHeight || document.documentElement.clientHeight) + 'px'
+                }).observe(a);
+            else
+                var s = Fluid.utils.listenScroll(function () {
+                    Fluid.utils.elementInViewport(a, c) && (Fluid.utils.unlistenScroll(s), n());
+                });
+        } else
+            n();
+    },
+    waitElementLoaded: function (n, i) {
+        var e = 'undefined' != typeof window, t = e && !('onscroll' in window) || 'undefined' != typeof navigator && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent);
+        e && !t ? 'MutationObserver' in window ? new MutationObserver(function (e, t) {
+            document.getElementById(n) && (i(), t.disconnect());
+        }).observe(document, {
+            childList: !0,
+            subtree: !0
+        }) : document.addEventListener('DOMContentLoaded', function () {
+            i();
+        }) : i();
+    },
+    createScript: function (e, t) {
+        var n = document.createElement('script');
+        n.setAttribute('src', e), n.setAttribute('type', 'text/javascript'), n.setAttribute('charset', 'UTF-8'), n.async = !1, 'function' == typeof t && (window.attachEvent ? n.onreadystatechange = function () {
+            var e = n.readyState;
+            'loaded' !== e && 'complete' !== e || (n.onreadystatechange = null, t());
+        } : n.onload = t);
+        var i = document.getElementsByTagName('script')[0] || document.getElementsByTagName('head')[0] || document.head || document.documentElement;
+        i.parentNode.insertBefore(n, i);
+    },
+    createCssLink: function (e) {
+        var t = document.createElement('link');
+        t.setAttribute('rel', 'stylesheet'), t.setAttribute('type', 'text/css'), t.setAttribute('href', e);
+        var n = document.getElementsByTagName('link')[0] || document.getElementsByTagName('head')[0] || document.head || document.documentElement;
+        n.parentNode.insertBefore(t, n);
+    },
+    loadComments: function (e, t) {
+        var n = document.querySelector('#comments[lazyload]');
+        if (n) {
+            Fluid.utils.waitElementVisible(e, function () {
+                t(), n.removeAttribute('lazyload');
+            }, CONFIG.lazyload.offset_factor);
+        } else
+            t();
     }
-  },
-
-  /**
-   * Attach this as the event listeners
-   */
-  handleEvent: function() {
-    this.requestTick();
-  }
+}, Debouncer.prototype = {
+    constructor: Debouncer,
+    update: function () {
+        this.callback && this.callback(), this.ticking = !1;
+    },
+    requestTick: function () {
+        this.ticking || (requestAnimationFrame(this.rafCallback || (this.rafCallback = this.update.bind(this))), this.ticking = !0);
+    },
+    handleEvent: function () {
+        this.requestTick();
+    }
 };
