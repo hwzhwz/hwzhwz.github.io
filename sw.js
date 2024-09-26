41f0ce764023372fe9bca5d0468434e4675e76b0
hovans <honven20@gmail.co>
2024-09-26 16:57:41 +0800

    Site updated: 2024-09-26 16:57:41

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/index.html","c73d3384180d08899666579352ee6b50"]];
+var precacheConfig = [["/index.html","1261516d19afaadcaf4818c5066bc81b"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
